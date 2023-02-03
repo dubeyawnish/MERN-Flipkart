5d3eb9efb518f27581b4c11f8f70ab800eacbaab
Awnish Dubey <dubeyawnish6@gmail.com>
2023-02-03 12:10:43 +0530

    signup and logout working properly

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 import express from "express"
+import cors from 'cors';
 import Connection from './database/db.js'
 import DefaultData from './default.js'
+import Router from './routes/route.js'
+import bodyParser from "body-parser";
 
 const app =express();
-
+app.use(cors());
+app.use(bodyParser.json({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/',Router);
 
 const PORT=process.env.PORT || 8000;
 Connection();

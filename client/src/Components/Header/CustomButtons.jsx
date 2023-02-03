e43af0b705f34d4316496ae1416953bd68a19ecd
Awnish Dubey <dubeyawnish6@gmail.com>
2023-02-03 09:39:45 +0530

    toggle login and signup is ready

--- client/src/Components/Header/CustomButtons.jsx
+++ client/src/Components/Header/CustomButtons.jsx
@@ -1,5 +1,7 @@
 import { Box, Button, Typography, styled } from '@mui/material';
 import { ShoppingCart } from '@mui/icons-material';
+import LoginDialog from '../login/LoginDialog';
+import { useState } from 'react';
 
 const Wrapper = styled(Box)`
 display:flex;
@@ -30,15 +32,21 @@ height:32px;
 
 
 const CustomButtons = () => {
+const [open, setOpen] = useState(false);
+const openDialog=()=>{
+  setOpen(true);
+}
+
   return (
     <Wrapper>
-      <LoginButton variant="contained "> Login</LoginButton >
+      <LoginButton variant="contained "  onClick={() =>openDialog()}> Login</LoginButton >
       <Typography style={{ marginTop: 3, width: 135 }}>Become a Seller</Typography>
       <Typography style={{ marginTop: 3 }}>More</Typography>
       <Container>
               <ShoppingCart />
               <Typography>Cart</Typography>
       </Container>
+      <LoginDialog open={open} setOpen={setOpen} /> 
 
 
     </Wrapper >

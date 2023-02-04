fc471dda512ab39e2349ac0af9df52d723b75370
Awnish Dubey <dubeyawnish6@gmail.com>
2023-02-05 00:21:26 +0530

    onclick product item displayed

--- client/src/Components/details/DetailsView.jsx
+++ client/src/Components/details/DetailsView.jsx
@@ -3,36 +3,82 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getProductDetails } from "../../redux/actions/productActions";
+import { styled, Box, Typography, Grid } from '@mui/material';
+import ActionItem from "./ActionItem";
+
+
+const Component = styled(Box)`
+    margin-top: 55px;
+    background: #F2F2F2;
+`;
+
+const Container = styled(Grid)(({ theme }) => ({
+    background: '#FFFFFF',
+    display: 'flex',
+    
+    [theme.breakpoints.down('md')]: {
+        margin: 0
+    }
+}))
+
+const RightContainer = styled(Grid)`
+    margin-top: 50px;
+    & > p {
+        margin-top: 10px;
+    }
+`;
+
 
 
 const DetailsView=()=>{
-const dispatch=useDispatch();
+        const fassured = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png'
+        const dispatch=useDispatch();
 
-// if we want to take any thing from website url then we need useParams
-const { id }=useParams();
-
-
-const {loading,product}= useSelector(state=>state.getProductDetails);
+        // if we want to take any thing from website url then we need useParams
+        const { id }=useParams();
 
 
-
-
-
-useEffect(() => {
-   if (product && id !== product.id)
-    dispatch(getProductDetails(id));
-
-  
-}, [dispatch,id,product,loading])
+        const {loading,product}= useSelector(state=>state.getProductDetails);
 
 
 
 
-    return (
-        <div>
-           <h1>hello</h1> 
-        </div>
-    )
+
+        useEffect(() => {
+        if (product && id !== product.id)
+            dispatch(getProductDetails(id));
+
+        
+        }, [dispatch,id,product,loading])
+
+
+
+
+            return (
+                <Component>
+                <Box></Box>
+                { product && Object.keys(product).length &&
+                    <Container container> 
+                        <Grid item lg={4} md={4} sm={8} xs={12}>
+                            <ActionItem product={product} />
+                        </Grid>
+                        <RightContainer item lg={8} md={8} sm={8} xs={12}>
+                            <Typography>{product.title.longTitle}</Typography>
+                            <Typography style={{marginTop: 5, color: '#878787', fontSize: 14 }}>
+                                8 Ratings & 1 Reviews
+                                <span><img src={fassured} style={{width: 77, marginLeft: 20}} /></span>
+                            </Typography>
+                            <Typography>
+                                <span style={{ fontSize: 28 }}>₹{product.price.cost}</span>&nbsp;&nbsp;&nbsp; 
+                                <span style={{ color: '#878787' }}><strike>₹{product.price.mrp}</strike></span>&nbsp;&nbsp;&nbsp;
+                                <span style={{ color: '#388E3C' }}>{product.price.discount} off</span>
+                            </Typography>
+                           {/* <ProductDetail product={product} /> */}
+                        </RightContainer>
+                    </Container>
+                }   
+            </Component>
+            )
 }
 
 

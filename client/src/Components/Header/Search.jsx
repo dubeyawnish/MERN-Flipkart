9eca885d633edd069628ecdcedf3cdd599913525
Awnish Dubey <dubeyawnish6@gmail.com>
2023-02-05 10:04:47 +0530

    just search and get the product

--- client/src/Components/Header/Search.jsx
+++ client/src/Components/Header/Search.jsx
@@ -1,5 +1,9 @@
-import { InputBase, Box, styled } from '@mui/material'
+import { InputBase, List, ListItem, Box, styled } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search';
+import { useState,useEffect } from 'react';
+import {useSelector,useDispatch} from 'react-redux'
+import { getProducts } from '../../redux/actions/productActions';
+import {Link} from 'react-router-dom'
 
 const StyledContainer = styled(Box)`
  background:#fff;
@@ -16,6 +20,15 @@ width:100%;
 font-size:unset;
 `;
 
+const ListWrapper = styled(List)`
+  position: absolute;
+  color: #000;
+  background: #FFFFFF;
+  margin-top: 36px;
+`;
+
+
+
 const SearchIconWrapper = styled(Box)`
 color:blue;
 padding:5px;
@@ -23,12 +36,46 @@ display:flex;
 `
 
 const Search = () => {
+const [text, setText] = useState('');
+const [ open, setOpen ] = useState(true)
+
+const getText=(text) =>{
+    setText(text);
+    setOpen(false);
+}
+const {products}  =useSelector(state=>state.getProducts);
+const dispatch=useDispatch();
+  useEffect(()=>{
+   dispatch(getProducts())
+  },[dispatch])
+
     return (
         <StyledContainer>
-            <InputSearchBase placeholder='Search for products, brands and more' />
+            <InputSearchBase placeholder='Search for products, brands and more'
+                             onChange={(e)=>getText(e.target.value)}
+            />
                 <SearchIconWrapper>
                      <SearchIcon />
                 </SearchIconWrapper>
+
+                {
+              text && 
+              <ListWrapper hidden={open}>
+                {
+                  products.filter(product => product.title.longTitle.toLowerCase().includes(text.toLowerCase())).map(product => (
+                    <ListItem>
+                      <Link 
+                        to={`/product/${product.id}`} 
+                        style={{ textDecoration:'none', color:'inherit'}}
+                        onClick={() => setOpen(true)}  
+                      >
+                        {product.title.longTitle}
+                      </Link>
+                    </ListItem>
+                  ))
+                }  
+              </ListWrapper>
+            }
 
         </StyledContainer>
 
